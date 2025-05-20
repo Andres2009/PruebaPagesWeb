26c2a9317d61d23a323dda8c031505667fb464c6
Andres Yodera <52515863+Andres2009@users.noreply.github.com>
2025-05-20 15:51:51 -0500

    as
    sa

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -9,17 +9,20 @@ export const LanguageProvider = ({ children }) => {
 
   const getProductos = async () => {
     try {
-      const res = await axios.get('https://credimovil.7-24.co/apirest/consumo/?token=uq5-HvDeRU63_ENR&objeto=productos');
-      console.log(res.data);
-    } catch (error) {
-      console.error('Error al obtener productos:', error.message);
-      if (error.response) {
-        console.error('Detalles del servidor:', error.response.data);
-      } else if (error.request) {
-        console.error('No hubo respuesta del servidor.');
-      } else {
-        console.error('Error desconocido:', error);
+      const response = await fetch('https://credimovil.7-24.co/apirest/consumo/?token=uq5-HvDeRU63_ENR&objeto=productos');
+      console.log(response);
+
+      if (!response.ok) {
+        const errorData = await response.text();
+        throw new Error(`Error del servidor: ${response.status} - ${errorData}`);
       }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+
+      console.error('Error al obtener productos:', error.message);
     }
   };
 
